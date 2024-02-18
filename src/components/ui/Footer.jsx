5acd3908958451c33902cbd7b92dbc4916da5bbc
unnatix <unnatig.ds@gmail.com>
2024-02-18 23:29:33 +0530

    upd

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="flex flex-col md:flex-row md:w-[62.5vw] lg:w-[57.5vw] justify-between">
         <div className="flex space-x-1" >
           <span>&copy;</span>
-          <span>2023</span>
+          <span>2024</span>
           <span className="font-extrabold uppercase 2xl:text-body-1">Unnati</span>
         </div>
         <div>
