e7260efd584824bccdbfe0baebd931c039ee0869
unnatix <124669944+unnatix@users.noreply.github.com>
2024-11-04 23:59:41 +0100

    up

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import profileImg from "/src/assets/images/profile.png";
+import profileImg from "/src/assets/images/profile.webp";
 import { ScrollTrigger } from "gsap/all";
 import { gsap } from "gsap";
 import Heading from "../ui/Heading";
